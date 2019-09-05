49ea325ca95a2102db6bfaeff0481a9efaed44e3
Kamil <kamil_p7@o2.pl>
2019-09-05 07:22:45 +0200

    src folder name change

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from './app/store/index';
-///import SearchScreen from './app/screens/SearchScreen';
-import AppNavigation from './app/navigation/navigation';
+import {store} from './src/store/index';
+import AppNavigation from './src/navigation/navigation';
 
 export default function App() {
   return (
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 
-//import SearchScreen from './app/screens/SearchScreen';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
